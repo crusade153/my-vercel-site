7ced0d684688b30113c28997d7c8ac4aaeff4db2
kdyu <crusade153@gmail.com>
2025-12-27 17:13:23 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>안녕하세요! Vercel 첫 걸음입니다.</h1>
+      <p>구글 앱스스크립트보다 훨씬 빠르죠?</p>
+    </div>
+  );
+}
